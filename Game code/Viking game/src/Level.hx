30ad9abb15e304c52219af4b0df2e7646e2e14a3
vkichev <vasilkichev@gmail.com>
2016-12-21 00:42:32 +0100

    Now it actually works
    Last time I was only checking the Y coordinate, so if you placed a
    single block at the beginning, the whole line would be considered as
    full. Now we check the whole array and can properly fall through holes
    and climb stairs.

--- Game code/Viking game/src/Level.hx
+++ Game code/Viking game/src/Level.hx
@@ -2,12 +2,10 @@ package src;
 
 import openfl.Assets;
 import openfl.display.Sprite;
-//import flash.events.Event;
 import openfl.events.KeyboardEvent;
 import openfl.geom.Point;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
-import openfl.geom.Rectangle;
 
 class Level extends Sprite
 {
@@ -30,20 +28,19 @@ class Level extends Sprite
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1]];
 		
 		block = new Sprite();
-		block.graphics.beginFill(0x3498db);
 		for (row in 0...map.length) {
 			for (coll in 0...map[row].length) {
 				if (map[row][coll] == 1){
-					//block.graphics.drawRect(coll * gridSize, row * gridSize, gridSize, gridSize);
+					
 					var blockData:BitmapData = Assets.getBitmapData( "img/block.png" );
 					blockBitmap = new Bitmap( blockData );
 					blockBitmap.x = coll * gridSize;
--- Game code/Viking game/src/Player.hx
+++ Game code/Viking game/src/Player.hx
@@ -12,65 +12,53 @@ import src.Level;
 class Player extends Sprite
 {
 	
-	private var gravity:Float = .1;
+	private var gravity:Float = 1.5;
 	private var jumpHeight:Float = 15.0;
-	private var velocity:Point = new Point(0, 0);
-	private var keys:Array<Bool>;
-	private var isOnGround:Bool;
-	private var jumped:Bool = false;
+	private var velocity:Point = new Point(0, 0); // Variable where we store the velocity to be added to the player. This is applied at the end of every frame loop.
+	private var keys:Array<Bool>; //Array in which we store the keyboard keys and their values for pressed or not.
+	private var isOnGround:Bool; //Are we colliding?
+	private var jumped:Bool = false; //To keep track if we're in the air from a jump
 	private var playerBitmap:Bitmap;
-	private var counter:Int = 0;
-	var canSpacebar:Bool = true;
-	var level : Level;
+	var canSpacebar:Bool = true; //Can we press spacebar again? So we don't do the infinte hop
+	var level : Level; //Referencing the level class, so we can read from it
 	
 	//run once upon creation
 	public function new() 
 	{
 		super();
-		level = new Level();
-		var playerData:BitmapData = Assets.getBitmapData( "img/Viking.png" );
+		
+		level = new Level(); //Reference to the level
+		
+		//Assigning the player's texture
+		var playerData:BitmapData = Assets.getBitmapData( "img/Viking.png" ); 
     	playerBitmap = new Bitmap( playerData );
 		
+		//We don't have to do this, but he looks much better. Draws the focus to him instead of the ground or background
 		playerBitmap.scaleX = 2;
 		playerBitmap.scaleY = 2;
 		
-		addChild(playerBitmap);
+		addChild(playerBitmap); //Adding the player sprite to the scene
 		
-		keys = [];
-		this.addEventListener(Event.ENTER_FRAME, everyFrame);
+		keys = []; //Defining the array we use to store the keyboard keys. It's empty because it's populated by the respective functions
 		
+		this.addEventListener(Event.ENTER_FRAME, everyFrame); //The game is frame based, so we're tracking things that happen every frame in the everyFrame function
 	}
 	
 	//Code that is run every frame
 	function everyFrame(evt:Event):Void
 	{
-		velocity.y += gravity;
-		/*
-		//Ground 
-		if (playerBitmap.y < 300) //When in the air
-		{
-			velocity.y += gravity;
-			isOnGround = false;
-		} 
-		else //When on ground
-		{
-			velocity.y = 0;
-			playerBitmap.y = 300;
-			isOnGround = true;
-		} */
+		velocity.y += gravity; //Applying the acceleration of gravity to the player. This happens constantly and is only contradicted by the collision functions
 		
 		//Movement
 		if (keys[39]) //Moving Right
 		{
-			playerBitmap.scaleX =  2;
-			velocity.x = 7; 
-			
+			//playerBitmap.scaleX =  2;
+			velocity.x = 7; //Movement speed
 		}
 		else if (keys[37]) //Moving Left
 		{
-			playerBitmap.scaleX = -2;
-			
-			velocity.x = -7;
+			//playerBitmap.scaleX = -2; //Bad implementation someone please fix it
+			velocity.x = -7; //Movement speed
 		}
 		else //Not Moving
 		{
@@ -78,14 +66,12 @@ class Player extends Sprite
 		}
 		
 		//Jumping
-		if (keys[32] && isOnGround && !jumped ) 
+		if (keys[32] && isOnGround && !jumped ) //If the spacebar is pressed and we're on the ground and we have not already jumped
 		{
-			velocity.y = -jumpHeight;
-			jumped = true;
-			//trace(jumpPress);
+			velocity.y = -jumpHeight; //Negative velocity is applied to the player, so they move up
+			jumped = true; //Keeping track we've jumped
+			
 		}
-	
-		
 		
 		//Making so that player has to relese SPACE to jump again
 		if (!keys[32] && jumped)
@@ -93,14 +79,14 @@ class Player extends Sprite
 			jumped = false;
 		}
 		
-		var tileCoords:Point = new Point(0, 0);//Where the tile we're moving into is located based on the grid
-		var approximateCoords:Point = new Point();//Where the player is located based on the level grid
+		var tileCoords:Point = new Point(0, 0); //Where the tile we're moving into is located based on the grid
+		var approximateCoords:Point = new Point(); //Where the player is located based on the level grid
 		
 		//Applying the velocities to the player
-		playerBitmap.y += velocity.y;//Apply the y velocity to the player
-		checkBottomCollision(tileCoords, approximateCoords);//Apply bottom collision
+		playerBitmap.y += velocity.y;	//Apply the y velocity to the player
+		checkBottomCollision(tileCoords, approximateCoords);	//Apply bottom collision
 		
-		playerBitmap.x += velocity.x;//Apply the x velocity to the player
+		playerBitmap.x += velocity.x;	//Apply the x velocity to the player
 		
 		if (velocity.y != 0) isOnGround = false; //Infinite jumping without this, since we removed the ground check in the beginning
 	}
@@ -111,29 +97,32 @@ class Player extends Sprite
 		if (velocity.y >= 0) 
 		{//If we're falling
 			
-			//Turning the player's actual coordinates to ones based on our grid
-			approximateCoords.y = (playerBitmap.y + playerBitmap.height/2) / (level.gridSize);
-			//trace("approximateCoords.y" + approximateCoords.y);
+			/*Turning the player's actual coordinates to ones based on our grid
+				First half of this equasion sets the collision point as the bottom of our character.
+				We then divide it by the gridsize to turn it into a value relative to our grid, so we can compare it to the blocks later.	*/
+			approximateCoords.y = (playerBitmap.y + playerBitmap.height / 2) / level.gridSize; 
+			approximateCoords.x = (playerBitmap.x + playerBitmap.width / 2) / level.gridSize; 
 			
 			tileCoords.y = Math.ceil(approximateCoords.y); 
+			tileCoords.x = Math.floor(approximateCoords.x); 
 			//Round up, which is actually down on the screen, so the bottom of the block the player is in, which is the top of the block we're coliding with
 			
 			if (isBlock(tileCoords)) { //If the tile we're going into is a block
 				playerBitmap.y = (tileCoords.y) * level.gridSize - playerBitmap.height; //Snap the player above the block. The weird math is to say how much above the block
-				trace(playerBitmap.y);
+				
 				velocity.y = 0; //Reset the player's velocity
 				isOnGround = true;			
 			}
 			
 			//We do this again because we often collide with 2 blocks at once
-			/*
-			tileCoords.x = Math.ceil(approximateCoords.x);
+			
+			//tileCoords.x = Math.ceil(approximateCoords.x);
 			
 			if (isBlock(tileCoords)) {
-				playerBitmap.y = (tileCoords.y - 1) * level.gridSize;
+				playerBitmap.y = (tileCoords.y ) * level.gridSize - playerBitmap.height;
 				velocity.y = 0;
 				isOnGround = true;
-			} */
+			} 
 		} 
 	}
 	
